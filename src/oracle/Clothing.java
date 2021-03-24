dafeb7c6ca45aa9eec664fff4f6f875979fa83e4
Paulina <paulina.krawczyk@hotmail.com>
2021-03-24 23:13:32 +0100

    8.1

--- src/oracle/Clothing.java
+++ src/oracle/Clothing.java
@@ -1,16 +1,16 @@
 package oracle;
 
 public class Clothing {
-	private final double MIN_PRICE = 10.0;
-	private final double MIN_TAX = 0.2;
+	public final static double MIN_PRICE = 10.0;
+	public final static double MIN_TAX = 0.2;
 	private String description;
 	private double price;
 	private String size = "M";
 
-	public Clothing(String description, double price, String size) {
+	public Clothing(String description, double price, String aSize) {
 		this.description = description;
 		this.price = price;
-		this.size = size;
+		size = aSize;
 	}
 
 	public String getDescription() {
@@ -27,7 +27,7 @@ public class Clothing {
 
 	public void setPrice(double price) {
 		this.price = Math.max(price, MIN_PRICE);
-//		this.price=  (price > MIN_PRICE) ? price : MIN_PRICE;
+//		this.price = (price > MIN_PRICE) ? price : MIN_PRICE;
 	}
 
 	public String getSize() {
@@ -36,5 +36,9 @@ public class Clothing {
 
 	public void setSize(String size) {
 		this.size = size;
+	}
+@Override
+	public String toString() {
+		return getDescription() + ", " + getSize() + ", " + getPrice();
 	}
 }
--- src/oracle/ShopApp.java
+++ src/oracle/ShopApp.java
@@ -2,15 +2,13 @@ package oracle;
 
 public class ShopApp {
 	public static void main(String[] args) {
-		double tax = 0.2;
-		double total = 0;
 
 		System.out.println("Welcome to Duck Choice Shop");
+		System.out.println("minimum price = " + Clothing.MIN_PRICE + " Tax = " + Clothing.MIN_TAX);
 		Customer c1 = new Customer("Picky", 3);
 		Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
 		Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
 		Clothing item3 = new Clothing("Green Scarf", 5, "S");
-
 		Clothing[] items = {item1, item2, item3, new Clothing("Blue T-Shirt", 10.5, "S")};
 
 		c1.addItems(items);
@@ -18,8 +16,25 @@ public class ShopApp {
 		System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
 
 		for (Clothing item : c1.getItems()) {
-			System.out.println("Items: " + item.getDescription());
+			System.out.println("Items: " + item);
+		}
+		int average = 0;
+		int count = 0;
+		for (Clothing item : c1.getItems()) {
+			if (item.getSize().equals("L")) {
+				count++;
+				average += item.getPrice();
+			}
 		}
 
+//		try {
+//			average = average / count;
+//			System.out.println("Average price " + average + ", Count " + count);
+//		} catch (ArithmeticException e) {
+//			System.out.println("Don't divide by zero");
+//		}
+
+		average = (count == 0) ? 0 : average / count;
+		System.out.println("Average price " + average + ", Count " + count);
 	}
 }
